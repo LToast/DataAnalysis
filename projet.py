d9a88102410fe709746fe9494a0624f0a7d7c927
Lineal <linealsp@gmail.com>
2019-02-25 23:28:29 +0100

    update join with scrap

--- projet.py
+++ projet.py
@@ -4,11 +4,13 @@ Created on Sat Feb 23 12:00:10 2019
 
 @author: Theo
 """
+from sklearn.decomposition import pca
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 #récupération de toutes les données
+scrap = pd.read_csv("scrap.csv")
 tracks_spotify =  pd.read_csv('track_details_spot.csv')
 dataset = pd.read_csv('dataLight.csv')
 #selection de la region
@@ -24,6 +26,19 @@ dfr_unique=dfr.get(['Artist','Track Name','URL']).drop_duplicates(subset=['Track
 agg_title=aggregation.join(dfr_unique.reset_index().set_index("Track Name"))
 #on extrait le track id de l'url pour la jointure
 agg_title["id"]=agg_title["URL"].str.split("/").str[4]
+full = agg_title.merge(scrap, on="id").select_dtypes([np.number])
+full["Score"] = full.Date / full.Position
+full = full.drop(["Unnamed: 0", "Position", "Date"], axis=1)
+
+
+full.plot(kind="scatter", x="Score", y="Streams")
+full  = full[full["Score"]<10]
+full.Score.plot(kind="hist")
+
+Score = full.Score.values
+plt.hist((Score - np.mean(Score)) / np.std(Score), bins=400);
+
+desc = full.describe()
 #jointure ave le fichier d'analyse des chansons
 join = agg_title.reset_index().set_index("id").join(tracks_spotify.set_index("id")).dropna()
 df=join.drop(["URL","Unnamed: 0","analysis_url","deezer_id","track_href","type","uri"],axis=1)
