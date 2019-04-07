67a52d67739cbc2d5478351dcc2095602b44ef24
Theo <theo.peuckert@gmail.com>
2019-04-07 16:20:03 +0200

    boîte à moustache lol

--- analyse.py
+++ analyse.py
@@ -7,8 +7,12 @@ Created on Fri Apr  5 18:28:30 2019
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 tracks = pd.read_csv('dataframe.csv',sep=',',encoding='latin1')
-tracks_numerical=tracks.drop(['Artist','id','Track Name'],axis=1)
+tracks_numerical=tracks.drop(['Artist','Track Name'],axis=1)
 tracks_numerical.describe()
-tracks_numerical.boxplot(column='acousticness',by='score')
+tracks_numerical.boxplot(column='duration_ms')
+tracks_numerical.boxplot(column='tempo')
+tracks_numerical.boxplot(column='Score')
